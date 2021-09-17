6980fb0359f1da0e05be94256a038669ea629a11
devlulcas <lucasalvesrego.contato@gmail.com>
2021-09-17 20:28:52 -0300

    add: comments and more elements selected

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-const colorPickerContainers = ["logo--color", "message--color","label--color"];
+// Seleciona todas as divs onde serão criados os color pickers
+// então cria os objetos para a criação dos cp com os ids dessas divs
+// logo depois são criados os color pickers
+// e eles são separados em constantes individuais
+const colorPickerContainers = ["logo--color", "message--color", "label--color"];
 
-const colorPickerConfigs = colorPickerContainers.map(container => colorPickerConfig(container));
+const colorPickerConfigs = colorPickerContainers.map((container) =>
+  colorPickerConfig(container)
+);
 
-const colorPickers = colorPickerConfigs.map(config => Pickr.create(config));
+const colorPickers = colorPickerConfigs.map((config) => Pickr.create(config));
+const logoColorPicker = colorPickers[0];
+const messageColorPicker = colorPickers[1];
+const labelColorPicker = colorPickers[2];
 
-const {logoColorPicker, messageColorPicker, labelColorPicker} = [...colorPickers];
+// Seleciona todos os inputs de texto
+const logoInput = document.querySelector("#logo");
+const labelInput = document.querySelector("#label");
+const messageInput = document.querySelector("#message");
 
+// Seleciona elementos de visualização
+const imagePreview = document.querySelector("#preview__img");
+const spanUrl = document.querySelector("#final__url");
+
+// Gera um objeto com basicamente a mesma config, se diferenciando apenas no id
 function colorPickerConfig(elementId) {
   return {
     el: `#${elementId}`,
